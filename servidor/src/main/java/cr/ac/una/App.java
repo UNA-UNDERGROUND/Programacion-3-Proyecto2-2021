d369c4dcae69903de80f86b81037d24c447ecce0
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-11 21:18:18 -0600

    Enviar netstrings

--- servidor/src/main/java/cr/ac/una/App.java
+++ servidor/src/main/java/cr/ac/una/App.java
@@ -4,14 +4,14 @@ import cr.ac.una.net.Server;
 import cr.ac.una.net.io.PacketHandler;
 import cr.ac.una.net.io.PacketProcessor;
 import cr.ac.una.net.io.ReadWriteHandler;
-import cr.ac.una.net.packet.handler.SimplePacketHandler;
+import cr.ac.una.net.packet.handler.NetStringPacketHandler;
 import cr.ac.una.net.packet.processor.EchoProcessor;
 
 public final class App {
     public static void main(String[] args) {
         try {
             PacketProcessor processor = new EchoProcessor();
-            PacketHandler packetHandler = new SimplePacketHandler(processor);
+            PacketHandler packetHandler = new NetStringPacketHandler(processor);
             ReadWriteHandler rwHandler = new ReadWriteHandler(packetHandler);
             new Server().start(rwHandler);
         } catch (InterruptedException e) {
