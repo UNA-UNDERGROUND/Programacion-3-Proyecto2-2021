4acedd18ad8178562a3898a86875026c8620035d
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-14 14:13:08 -0600

    recibir netstrings

--- servidor/src/main/java/cr/ac/una/net/packet/handler/NetStringPacketHandler.java
+++ servidor/src/main/java/cr/ac/una/net/packet/handler/NetStringPacketHandler.java
@@ -4,6 +4,13 @@ import cr.ac.una.net.io.PacketHandler;
 import cr.ac.una.net.io.PacketProcessor;
 import cr.ac.una.net.io.SocketData;
 import cr.ac.una.net.packet.netstring.NetString;
+import cr.ac.una.net.packet.netstring.exception.CharacterNotExpectedException;
+import cr.ac.una.net.packet.netstring.exception.DataDelimiterNotFoundException;
+import cr.ac.una.net.packet.netstring.exception.InvalidNumberException;
+import cr.ac.una.net.packet.netstring.exception.LengthDelimiterNotFoundException;
+import cr.ac.una.net.packet.netstring.exception.NegativeLengthException;
+import cr.ac.una.net.packet.netstring.exception.NetStringFormatException;
+import cr.ac.una.net.packet.netstring.exception.NotEnoughtBytesException;
 
 public class NetStringPacketHandler extends ProcessorPacketHandler implements PacketHandler {
 
@@ -14,31 +21,34 @@ public class NetStringPacketHandler extends ProcessorPacketHandler implements Pa
     @Override
     public int processPacket(String buffer, SocketData client) {
         try {
-            // verificamos si hay una nueva linea
-            if (buffer.contains("\n")) {
-                // obtenemos el indice de la linea
-                int index = buffer.indexOf("\n");
-                // verificamos si tiene un retorno de carro
-                boolean hasCR = buffer.charAt(index - 1) == '\r';
-                // obtenemos el desplazamiento sensible al retorno de carro
-                // obtenemos la linea
-                String linea;
-                // si la nueva linea es el inicio entonces esta vacio
-                if (index == 0 || (hasCR && index == 1)) {
-                    linea = "";
-                } else {
-                    // obtenemos la linea sin el salto de linea
-                    linea = buffer.substring(0, index - (hasCR ? 1 : 0));
-                }
-                // removemos la linea nueva del mensaje
-                // y eliminamos el mensaje de la lista
-                int bytes = linea.length() + (hasCR ? 2 : 1);
-                processor.process(linea, client, this);
-                // retornamos la cantidad de bytes recolectados
-                return bytes;
-            }
+            // primero intentamos recibir el netstring
+            // si no es posible veremos si es un error faltal o no
+            // con la excepcion lanzada
+            NetString netString = NetString.parseNetString(buffer);
+            // luego procesamos su valor
+            processor.process(netString.getData(), client, this);
+            // los bytes leidos es equivalente a la longitud del netstring
+            return netString.toString().length();
+        }
+        // faltan mas bytes por leer no es un error fatal
+        catch (NotEnoughtBytesException | LengthDelimiterNotFoundException ex) {
             return 0;
-        } catch (Exception e) {
+        }
+        // un caracter no esperado es un error fatal
+        catch (CharacterNotExpectedException | DataDelimiterNotFoundException | NegativeLengthException
+                | InvalidNumberException ex) {
+            System.err.println("Error de veficacion: " + ex.getMessage());
+            return -1;
+        }
+        // error desconocido de netstring
+        catch (NetStringFormatException e) {
+            System.err.println("Error de protocolo: " + e.getMessage());
+            // y retornamos -1 para indicar que es un error desconocido
+            return -1;
+        }
+        // excepcion desconocida // error faltal
+        catch (Exception e) {
+            System.err.println("Error desconocido: " + e.getMessage());
             return -1;
         }
     }
