41fca35edaaf7f7acb96c6755f40e82c94390bd3
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-15 08:49:19 -0600

    monto

--- assets/script.sql
+++ assets/script.sql
@@ -1,9 +1,19 @@
 create schema cajeros;
-
-create table usuarios
-(
-    usuario varchar(255) not null
-        primary key,
-    pass    varchar(100)   not null
+-- creacion de tablas
+create table usuarios (
+    usuario varchar(255) not null,
+    pass varchar(100) not null
 );
-
+create table cuenta(
+    id_cuenta int not null,
+    usuario varchar(255) not null,
+    saldo decimal(15, 2) not null,
+);
+-- creacion de llaves primarias
+alter table usuarios
+add constraint pk_usuarios primary key (usuario);
+alter table cuenta
+add constraint pk_cuenta primary key (id_cuenta);
+-- creacion de llaves foraneas
+alter table cuenta
+add constraint fk_cuenta_usuario foreign key (usuario) references usuarios(usuario) on delete cascade;
