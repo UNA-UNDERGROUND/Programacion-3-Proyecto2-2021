0c60ecaee6f95dca803c026ccd5da6b96073c658
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-15 17:47:06 -0600

    controlador del cliente

---
+++ cliente/src/main/java/cr/ac/una/controller/GeneralController.java
@@ -0,0 +1,30 @@
+package cr.ac.una.controller;
+
+import cr.ac.una.model.ClientConnection;
+
+public class GeneralController {
+
+    public boolean login(String user, String password) {
+        if (connection == null) {
+            connection = new ClientConnection(user, password);
+        }
+        if (connection.connect(host, port)) {
+            return connection.auth();
+        }
+        return false;
+    }
+
+    private ClientConnection connection;
+
+    private static final String host = "localhost";
+    private static final int port = 5721;
+    // singleton
+    private static GeneralController instance = null;
+
+    public static GeneralController getInstance() {
+        if (instance == null) {
+            instance = new GeneralController();
+        }
+        return instance;
+    }
+}
