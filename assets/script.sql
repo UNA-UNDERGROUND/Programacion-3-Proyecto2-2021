df7b32b6ad77cdbc81de479e3e10a5ca4e6275b4
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-15 19:16:03 -0600

    valor por defecto saldo

--- assets/script.sql
+++ assets/script.sql
@@ -2,18 +2,7 @@ create schema cajeros;
 -- creacion de tablas
 create table usuarios (
     usuario varchar(255) not null,
-    pass varchar(100) not null
+    pass varchar(100) not null,
+    saldo DECIMAL(15,2) not null default 0
 );
-create table cuenta(
-    id_cuenta int not null,
-    usuario varchar(255) not null,
-    saldo decimal(15, 2) not null,
-);
--- creacion de llaves primarias
-alter table usuarios
-add constraint pk_usuarios primary key (usuario);
-alter table cuenta
-add constraint pk_cuenta primary key (id_cuenta);
--- creacion de llaves foraneas
-alter table cuenta
-add constraint fk_cuenta_usuario foreign key (usuario) references usuarios(usuario) on delete cascade;
+
